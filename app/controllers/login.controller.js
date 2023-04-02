8b2ad65547630bb338f6be95ec528b374bed32f6
lananh <anh626801@gmail.com>
2023-04-02 10:55:11 +0700

    jwt

---
+++ app/controllers/login.controller.js
@@ -0,0 +1,86 @@
+const con = require("../util/mysql.util");
+const ApiError = require("../api_error");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+const { rejects } = require("assert");
+const { Console } = require("console");
+
+exports.hienthi = (req, res, next) => {
+  res.sendFile(path.join(__dirname + "/../../views/", "index.html"));
+};
+exports.KTDN = async (req, res, next) => {
+  let token = req.cookies.token;
+  var TTTK;
+  try {
+    let kq = jwt.verify(token, "password");
+    if (kq) {
+      TTTK = await
+        con
+          .promise()
+          .query("select * from taikhoan where STT=?;", kq.STT)
+          .then((data) => {
+            return data[0];
+          })
+          .catch((err) => {
+            return err;
+          });
+    }
+  } catch (err) {
+    return res.redirect("/api/login");
+  }
+  req.data=Object(TTTK[0]);
+  next();
+};
+
+exports.checkChuTro=(req,res,next)=>{
+  
+  if(req.data.quyen==='1'){
+    next();
+  }else{
+    res.json('Khong co quyen');
+  }
+}
+exports.checkKhach=(req,res,next)=>{
+  
+  if(req.data.quyen==='0'){
+    next();
+  }else{
+    res.json('Khong co quyen');
+  }
+}
+exports.trangbatbuocDaDN = (req, res, next) => {
+  console.log(req.data);
+  res.json("welcome you to private page");
+};
+exports.dangnhap = (req, res, next) => {
+  // console.log(req.body);
+  let checkdangnhap =
+    "select STT from taikhoan where matk=? and matkhau=md5(?);";
+  try {
+    // console.log(req.body.matk,req.body.matkhau);
+    con.query(
+      checkdangnhap,
+      [req.body.matk, req.body.matkhau],
+      (err, result, field) => {
+        if (err) throw err.stack;
+        else if (result.length > 0) {
+          kq = Object(result[0]);
+
+          jwt.sign(result[0], "password", function (err, data) {
+            return res.json({
+              message: "thành công",
+              token: data,
+            });
+          });
+        } else {
+          res.send("đăng nhập thất bại");
+        }
+      }
+    );
+  } catch (error) {
+    return new ApiError(500, "đăng nhập lỗi");
+  }
+};
+
+
+
---
+++ app/routes/login.routes.js
@@ -0,0 +1,11 @@
+const express= require('express');
+const router=express.Router();
+const login=require('../controllers/login.controller');
+
+router.route('/')
+    .get(login.hienthi)
+    .post(login.dangnhap);
+router.route('/private')
+    .get([login.KTDN,login.checkChuTro,login.trangbatbuocDaDN]);
+
+module.exports=router;
