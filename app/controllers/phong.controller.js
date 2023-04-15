07f525a130ac10648374070b68419acdf45a42a1
lananh <anh626801@gmail.com>
2023-04-15 22:22:43 +0700

    api phong

--- app/controllers/phong.controller.js
+++ app/controllers/phong.controller.js
@@ -40,10 +40,10 @@ exports.themTT= (req,res,next)=>{
     let myquery="INSERT INTO `qlnhatro`.`phong` (`maloai`, `tenphong`, `trangthai`) VALUES (?, ?, '0');";
     try {
         con.query(myquery,
-            [req.query.maloai,req.body.tenphong],
+            [req.params.maloai,req.body.tenphong],
             function(err,result, field){
             if(err) throw err.stack;
-            return res.send('Thêm phòng thành công');
+            return res.json({mes:`them phòng  mã loại ${req.params.maloai}`});
         })
     } catch (error) {
         return new ApiError(500,'Không kết nối với phòng');
@@ -59,6 +59,21 @@ exports.chinhsuaPhong= (req,res,next)=>{
             function(err,result, field){
             if(err) throw err.stack;
             return res.send('Chỉnh sửa trạng thái phòng thành công');
+        })
+    } catch (error) {
+        return new ApiError(500,'Không kết nối với phòng');
+    }
+
+}
+//Chỉnh sửa trạng thái phòng
+exports.xoaPhong= (req,res,next)=>{
+    let myquery="   DELETE FROM `qlnhatro`.`phong` WHERE (`maphong` = ?);";
+    try {
+        con.query(myquery,
+            [req.params.maphong],
+            function(err,result, field){
+            if(err) throw err.stack;
+            return res.json({mes:`xóa thành công phong ${req.params.maphong}`});
         })
     } catch (error) {
         return new ApiError(500,'Không kết nối với phòng');
--- app/routes/phong.routes.js
+++ app/routes/phong.routes.js
@@ -3,11 +3,13 @@ const phong= require('../controllers/phong.controller');
 const router= express.Router();
 
 router.route('/')
-        .get(phong.layTTP)
+        .get(phong.layTTP);
+router.route('/:maloai')
         .post(phong.themTT);
 router.route('/:maphong')
         .get(phong.layPhong)
-        .put(phong.chinhsuaPhong);
+        .put(phong.chinhsuaPhong)
+        .delete(phong.xoaPhong);
 router.route('/dsphong/:maloai')
 .get(phong.LTTPTheoLoai);
 module.exports=router;
